77ec76dcadffb47d8a11dff8efac3763a91e7350
tiyo <eduardotiyo0@gmail.com>
2025-12-29 20:03:49 -0300

    feat: adicionar header com breakpoints. adicionar clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@ import PostsList from "@/components/PostsList";
 import SpinLoader from "@/components/SpinLoader";
 import Container from "@/components/Container";
 import { Suspense } from "react";
+import Header from "@/components/Header";
 
 export default async function Home() {
   return (
     <Container>
-      <header>
-        <h1 className="text-6xl font-bold text-center py-8">HEADER</h1>
-      </header>
+      <Header />
       <p className="text-justify">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vero,
         voluptatibus ad minus id laudantium magni optio et impedit itaque iure
