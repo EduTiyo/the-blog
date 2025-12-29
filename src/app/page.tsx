dc096697493454691c7b388fb005d5cbec2a2856
tiyo <eduardotiyo0@gmail.com>
2025-12-29 20:38:46 -0300

    feat: esqueleto da ultima noticia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,43 @@ import SpinLoader from "@/components/SpinLoader";
 import Container from "@/components/Container";
 import { Suspense } from "react";
 import Header from "@/components/Header";
+import Link from "next/link";
+import Image from "next/image";
 
 export default async function Home() {
   return (
     <Container>
       <Header />
-      <p className="text-justify">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vero,
-        voluptatibus ad minus id laudantium magni optio et impedit itaque iure
-        deleniti exercitationem magnam possimus provident mollitia tempore
-        dolorum facilis? Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Corporis vero, voluptatibus ad minus id laudantium magni optio et
-        impedit itaque iure deleniti exercitationem magnam possimus provident
-        mollitia tempore dolorum facilis? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Corporis vero, voluptatibus ad minus id laudantium
-        magni optio et impedit itaque iure deleniti exercitationem magnam
-        possimus provident mollitia tempore dolorum facilis? Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Corporis vero, voluptatibus ad
-        minus id laudantium magni optio et impedit itaque iure deleniti
-        exercitationem magnam possimus provident mollitia tempore dolorum
-        facilis? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Corporis vero, voluptatibus ad minus id laudantium magni optio et
-        impedit itaque iure deleniti exercitationem magnam possimus provident
-        mollitia tempore dolorum facilis?
-      </p>
+
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl " href="#">
+          <Image
+            src="/images/bryen_0.png"
+            width={1200}
+            height={720}
+            alt="Título do post"
+            className="w-full h-full object-cover group-hover:scale-105 transition"
+            priority
+          />
+        </Link>
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time
+            className="text-muted-foreground block text-xs/tight"
+            dateTime="2025-29-12"
+          >
+            29/12/2025 20:22
+          </time>
+          <h1 className="text-2xl/tight font-extrabold sm:text-4xl/tight">
+            Lorem ipsum dolor sit amet.
+          </h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit, alias
+            nemo ab, vel aspernatur molestias fugit blanditiis quas natus at, a
+            deleniti soluta odit maxime laudantium aliquid nam unde mollitia.
+          </p>
+        </div>
+      </section>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
