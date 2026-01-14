f58dd7edb3cf5e966e4980b2b43b56266dc1fb56
tiyo <eduardotiyo0@gmail.com>
2026-01-14 15:06:58 -0300

    feat: componente e inicio da action de login

---
+++ src/app/admin/login/components/LoginForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { loginAction } from "@/actions/login/login-action";
+import { showMessage } from "@/adapters/showMessage";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { LogInIcon } from "lucide-react";
+import { useActionState, useEffect } from "react";
+
+const LoginForm = () => {
+  const initialState = {
+    username: "",
+    error: "",
+  };
+  const [state, action, isPending] = useActionState(loginAction, initialState);
+
+  useEffect(() => {
+    if (state.error) {
+      showMessage.dismiss();
+      showMessage.error(state.error);
+    }
+  }, [state]);
+
+  return (
+    <form action={action}>
+      <div className="flex flex-col gap-6 mb-6">
+        <div className="grid gap-2">
+          <Label htmlFor="username">Usuário</Label>
+          <Input
+            id="username"
+            type="text"
+            placeholder="Seu usuário"
+            required
+            disabled={isPending}
+            defaultValue={state.username}
+          />
+        </div>
+        <div className="grid gap-2">
+          <div className="flex items-center">
+            <Label htmlFor="password">Senha</Label>
+          </div>
+          <Input
+            id="password"
+            type="password"
+            required
+            placeholder="**********"
+            disabled={isPending}
+          />
+        </div>
+      </div>
+
+      <Button type="submit" className="w-full" disabled={isPending}>
+        <LogInIcon />
+        Entrar
+      </Button>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,8 +1,28 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import LoginForm from "./components/LoginForm";
+
 export const dynamic = "force-dynamic";
 
 const LoginPage = () => {
   return (
-    <div className="py-16 text-6xl/tight mx-auto text-center">LoginPage</div>
+    <Card className="w-full max-w-sm mx-auto mb-32 mt-16">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          Coloque seu usuário e senha para acessar o painel administrativo.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <LoginForm />
+      </CardContent>
+    </Card>
   );
 };
 
