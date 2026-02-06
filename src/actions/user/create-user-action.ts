014d25f958d401fd1a765ee500dc357c5aa9f894
tiyo <eduardotiyo0@gmail.com>
2026-02-06 16:28:41 -0300

    feat(cadastrar-user): validar campos do formulário

---
+++ src/actions/user/create-user-action.ts
@@ -0,0 +1,46 @@
+"use server";
+
+import {
+  CreateUserSchema,
+  PublicUserDto,
+  PublicUserSchema,
+} from "@/lib/user/schemas";
+import { asyncDelay } from "@/utils/async-delay";
+import { getZodErrorMessages } from "@/utils/get-zod-error-messages";
+
+type CreateUserActionState = {
+  user: PublicUserDto;
+  errors: string[];
+  success: Boolean;
+};
+
+export async function createUserAction(
+  state: CreateUserActionState,
+  formData: FormData,
+): Promise<CreateUserActionState> {
+  if (!(formData instanceof FormData)) {
+    return {
+      user: state.user,
+      errors: ["Dados inválidos"],
+      success: false,
+    };
+  }
+
+  const formObj = Object.fromEntries(formData.entries());
+  const parsedFormData = CreateUserSchema.safeParse(formObj);
+
+  if (!parsedFormData.success) {
+    const errors = getZodErrorMessages(parsedFormData.error);
+    return {
+      user: PublicUserSchema.parse(formObj),
+      errors,
+      success: false,
+    };
+  }
+
+  return {
+    user: state.user,
+    errors: [],
+    success: true,
+  };
+}
--- src/components/CreateUserForm/index.tsx
+++ src/components/CreateUserForm/index.tsx
@@ -1,11 +1,36 @@
+"use client";
+
 import { UserIcon } from "lucide-react";
 import { Button } from "../ui/button";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
+import { useActionState, useEffect } from "react";
+import { createUserAction } from "@/actions/user/create-user-action";
+import { PublicUserSchema } from "@/lib/user/schemas";
+import { showMessage } from "@/adapters/showMessage";
+import SpinLoader from "../SpinLoader";
 
 const CreateUserForm = () => {
+  const initialState = {
+    user: PublicUserSchema.parse({}),
+    errors: [],
+    success: false,
+  };
+
+  const [state, action, isPending] = useActionState(
+    createUserAction,
+    initialState,
+  );
+
+  useEffect(() => {
+    showMessage.dismiss();
+    if (state.errors.length > 0) {
+      state.errors.forEach((error) => showMessage.error(error));
+    }
+  }, [state]);
+
   return (
-    <form action={""}>
+    <form action={action}>
       <div className="flex flex-col gap-6 mb-6">
         <div className="grid gap-2">
           <Label htmlFor="name">Nome</Label>
@@ -15,20 +40,20 @@ const CreateUserForm = () => {
             type="text"
             placeholder="Seu nome"
             required
-            disabled={false}
-            defaultValue={""}
+            disabled={isPending}
+            defaultValue={state.user.name}
           />
         </div>
         <div className="grid gap-2">
-          <Label htmlFor="username">Usuário</Label>
+          <Label htmlFor="email">Usuário</Label>
           <Input
-            id="username"
-            name="username"
+            id="email"
+            name="email"
             type="text"
-            placeholder="Seu usuário"
+            placeholder="Seu email"
             required
-            disabled={false}
-            defaultValue={""}
+            disabled={isPending}
+            defaultValue={state.user.email}
           />
         </div>
         <div className="grid gap-2">
@@ -41,7 +66,7 @@ const CreateUserForm = () => {
             type="password"
             required
             placeholder="**********"
-            disabled={false}
+            disabled={isPending}
           />
         </div>
         <div className="grid gap-2">
@@ -54,14 +79,24 @@ const CreateUserForm = () => {
             type="password"
             required
             placeholder="**********"
-            disabled={false}
+            disabled={isPending}
           />
         </div>
       </div>
 
-      <Button type="submit" className="w-full" disabled={false}>
-        <UserIcon />
-        Entrar
+      <Button type="submit" className="w-full" disabled={isPending}>
+        {!isPending && (
+          <>
+            <UserIcon />
+            Criar conta
+          </>
+        )}
+        {isPending && (
+          <>
+            <SpinLoader containerClasses="w-5 h-5" />
+            Criando...
+          </>
+        )}
       </Button>
     </form>
   );
